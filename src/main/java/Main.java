d015c5f45b362b91a1ad288dbc981a548ad3103d
antonionduarte <an.duarte@campus.fct.unl.pt>
2022-10-12 17:15:37 +0000

    Changed to env variables setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(System.getenv("AES_KEY"));
+		// System.out.println(System.getenv("AES_KEY"));
+		System.out.println(System.getenv("CRYPTO_CONFIG_KEY"));
 	}
 }
