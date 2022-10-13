4573ce281cc6f9a9b2740d83749da0d2a07259e5
antonionduarte <an.duarte@campus.fct.unl.pt>
2022-10-13 22:52:10 +0000

    Test class to replace VLC in case it's not working (just checks if packets are being received from the proxy)

---
+++ src/main/java/VLCTest.java
@@ -0,0 +1,41 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.Properties;
+
+public class VLCTest {
+
+	private static final String CONFIG_PATH = "config/proxy/config.properties";
+	private static final String PROPERTY_DESTINATIONS = "localdelivery";
+
+	public static void main(String[] args) throws IOException {
+		InputStream inputStream = new FileInputStream(CONFIG_PATH);
+		Properties properties = new Properties();
+		properties.load(inputStream);
+		String destinations = properties.getProperty(PROPERTY_DESTINATIONS);
+
+		SocketAddress inSocketAddress = parseSocketAddress(destinations);
+		try (DatagramSocket inSocket = new DatagramSocket(inSocketAddress)) {
+			byte[] buffer = new byte[4 * 1024];
+			while (true) {
+				DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
+				inSocket.receive(inPacket);  // if remote is unicast
+
+				System.out.print("-");
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static InetSocketAddress parseSocketAddress(String socketAddress) {
+		String[] split = socketAddress.split(":");
+		String host = split[0];
+		int port = Integer.parseInt(split[1]);
+		return new InetSocketAddress(host, port);
+	}
+}
