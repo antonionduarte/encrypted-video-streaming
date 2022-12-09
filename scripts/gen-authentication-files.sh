19c0bf5bc5be49bdab105a67d6facc7a2c9ea864
daro.almeida <daro.almeida@campus.fct.unl.pt>
2022-12-09 16:09:42 +0000

    updated script for chains (not working)

--- scripts/gen-authentication-files.sh
+++ scripts/gen-authentication-files.sh
@@ -2,63 +2,100 @@
 #ex: RSA 2048
 
 config_folder="../config/"
-proxy_config_folder=$config_folder"proxy/"
+box_config_folder=$config_folder"box/"
 server_config_folder=$config_folder"server/"
+ca_config_folder=$config_folder"ca/"
 common_config_folder=$config_folder"common/"
 certs_folder="certs/"
-password="aaaaaaaabbbbbbbbccccccccdddddddd"
+csr_folder="csr/"
+ca_password="aaaaaaaabbbbbbbbccccccccdddddddd"
+box_password="aaaaaaaabbbbbbbbccccccccdddddddd"
+server_password="aaaaaaaabbbbbbbbccccccccdddddddd"
 
 alg=$(echo $1 | tr 'a-z' 'A-Z')
 key_size=$2
-proxy_alias="proxy_"$alg"_"$key_size
+ca_alias="CA_"$alg"_"$key_size
+box_alias="box_"$alg"_"$key_size
 server_alias="server_"$alg"_"$key_size
-proxy_ks=$proxy_config_folder"proxy.pkcs12"
-proxy_cert=$proxy_config_folder$certs_folder$proxy_alias".cer"
+ca_ks=$ca_config_folder"ca.pkcs12"
+ca_cert=$common_config_folder"ca_root.cer"
+box_ks=$box_config_folder"box.pkcs12"
+box_cert=$box_config_folder$certs_folder$box_alias".cer"
+box_csr=$box_config_folder$csr_folder$box_alias".csr"
 server_ks=$server_config_folder"server.pkcs12"
 server_cert=$server_config_folder$certs_folder$server_alias".cer"
-trustedstore=$common_config_folder"trustedstore"
+server_csr=$server_config_folder$csr_folder$server_alias".csr"
 
-#Generate key pair for proxy and server
+#To begin, we first generate a key pair which will be used as the CA,
+ #ts private key will be used to sign the certificate it issues.
 keytool -genkeypair -noprompt \
-  -dname "CN=Unknown, OU=Unknown, O=Unknown, L=Unknown, S=Unknown, C=Unknown" \
+  -dname "CN=CA" \
   -keysize $key_size \
   -keyalg $alg \
   -storetype PKCS12 \
-  -alias $proxy_alias \
-  -keystore $proxy_ks \
-  -storepass $password
+  -alias $ca_alias \
+  -keystore $ca_ks \
+  -storepass $ca_password \
+  -ext bc=ca:true
+
+#Generate CA root certificate
+keytool -gencert -noprompt \
+  -keystore $ca_ks \
+  -storepass $ca_password \
+  -storetype PKCS12 \aaaaaaaabbbbbbbbccccccccdddddddd
+  -alias $ca_alias \
+  -outfile $ca_cert
+
+#Then, generate a key pair where the certificate of it will be signed by the CA above.
 keytool -genkeypair -noprompt \
-  -dname "CN=Unknown, OU=Unknown, O=Unknown, L=Unknown, S=Unknown, C=Unknown" \
+  -dname "CN=Box" \
+  -keysize $key_size \
+  -keyalg $alg \
+  -storetype PKCS12 \
+  -alias $box_alias \
+  -keystore $box_ks \
+  -storepass $box_password
+keytool -genkeypair -noprompt \
+  -dname "CN=Server" \
   -keysize $key_size \
   -keyalg $alg \
   -storetype PKCS12 \
   -alias $server_alias \
   -keystore $server_ks \
-  -storepass $password
+  -storepass $server_password
 
-#Generate certificates for proxy and server
-keytool -export \
-  -alias $proxy_alias \
-  -keystore $proxy_ks \
-  -file $proxy_cert <<< $password
-keytool -export \
-  -alias $server_alias \
+#Next, a certificate request for the "CN=Leaf" certificate needs to be created.
+keytool -certreq -noprompt \
   -keystore $server_ks \
-  -file $server_cert <<< $password
-
-#Insert certificates in trustedstore (CA)
-keytool -import -noprompt \
+  -storepass $server_password \
   -storetype PKCS12 \
-  -file $proxy_cert \
-  -alias $proxy_alias \
-  -keystore $trustedstore <<< $password
-keytool -import -noprompt \
-  -storetype PKCS12 \
-  -file $server_cert \
   -alias $server_alias \
-  -keystore $trustedstore <<< $password
+  -file $server_csr
+keytool -certreq -noprompt \
+  -keystore $box_ks \
+  -storepass $box_password \
+  -storetype PKCS12 \
+  -alias $box_alias \
+  -file $box_csr
 
-#List trustedstore
-keytool -list -noprompt -v -keystore $trustedstore <<< $password
+#Now creating the certificate with the certificate request generated above.
+keytool -gencert -noprompt \
+  -keystore $ca_ks \
+  -storepass $ca_password \
+  -storetype PKCS12 \
+  -alias $box_alias \
+  -infile $box_csr \
+  -outfile $box_cert
+keytool -gencert -noprompt \
+  -keystore $ca_ks \
+  -storepass $ca_password \
+  -storetype PKCS12 \
+  -alias $server_alias \
+  -infile $server_csr \
+  -outfile $server_cert
 
+# An output certificate file leaf.cer will be created. Now let's see what its content is.
+keytool -printcert -file $ca_cert
+keytool -printcert -file $box_cert
+keytool -printcert -file $server_cert
 
