5b64f56530c3afbd66e256300d54d3b0af8c2a3f
antonionduarte <an.duarte@campus.fct.unl.pt>
2022-12-11 05:11:36 +0000

    signature tool with functions to create and verify signatures

---
+++ src/main/java/cryptotools/signatures/SignaturesTool.java
@@ -0,0 +1,37 @@
+package cryptotools.signatures;
+
+import java.security.*;
+import java.security.cert.X509Certificate;
+
+public class SignaturesTool {
+	/**
+	 * Verifies a digital signature using an X509 certificate.
+	 */
+	public static boolean verifySignature(byte[] signature, byte[] data, X509Certificate certificate) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+		PublicKey key = certificate.getPublicKey();
+
+		Signature sig = Signature.getInstance("SHA256withRSA");
+		sig.initVerify(key);
+
+		// Update the signature object with the data that was signed
+		sig.update(data);
+
+		// Verify the signature
+		return sig.verify(signature);
+	}
+
+	/**
+	 * Creates a digital signature.
+	 */
+	public static byte[] createSignature(byte[] data, PrivateKey key) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+		// Create a Signature object and initialize it with the private key
+		Signature sig = Signature.getInstance("SHA256withRSA");
+		sig.initSign(key);
+
+		// Update the signature object with the data that you want to sign
+		sig.update(data);
+
+		// Generate the signature
+		return sig.sign();
+	}
+}
